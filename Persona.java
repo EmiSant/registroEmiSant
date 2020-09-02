cda0b2c83f0096cbca6be2405a23b81c5c6af07a
EmiSant <64933853+EmiSant@users.noreply.github.com>
2020-09-02 19:24:04 -0300

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registro;
+
+/**
+ *
+ * @author SebastianR
+ */
+public class Persona {
+
+    int DNI;
+    int Edad;
+    String Nombre, Apellido;
+
+    public Persona() {
+        this.DNI = 0;
+        this.Nombre = "";
+        this.Apellido = "";
+        this.Edad = 12;
+    }
+
+    public Persona(int DNI) {
+        this();
+        this.DNI = DNI;
+    }
+
+    public Persona(int DNI, String Nombre) {
+        this(DNI);
+        this.Nombre = Nombre;
+
+    }
+
+    public Persona(int DNI, String Nombre, String Apellido) {
+        this(DNI, Nombre);
+        this.Apellido = Apellido;
+    }
+
+    public Persona(int DNI, String Nombre, String Apellido, int Edad) {
+        this(DNI, Nombre, Apellido);
+        this.Edad = Edad;
+    }
+
+    ; 
+    
+    public void mostrar() {
+
+        System.out.println(DNI + " " + Nombre + " " + Apellido);
+    }
+
+}
